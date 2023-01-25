c76da23862e8a9d9dd4c1feea3828d0c9ea4a472
Ironwolf <michaelvologodskiy@gmail.com>
2023-01-25 08:48:09 +0300

    Update MonotonicStack

--- Stack/MonotonicStack.py
+++ Stack/MonotonicStack.py
@@ -1,18 +1,30 @@
+import operator
+
+
 class MonotonicStack:
-    @staticmethod
-    def nearestSmallerElements(nums: list[int]) -> list[int | None]:
-        stack = []
+    def _nextElement(nums: list[int], smaller: bool) -> list[int | None]:
+        comp = operator.gt if smaller else operator.lt
         res = [None] * len(nums)
+        stack = []
 
         for i, el in enumerate(nums):
-            while stack and nums[stack[-1]] < el:
-                res[i] = nums[stack.pop()]
+            while stack and comp(nums[stack[-1]], el):
+                res[stack.pop()] = el
 
             stack.append(i)
 
         return res
 
+    @staticmethod
+    def nextSmallerElement(nums: list[int]) -> list[int | None]:
+        return MonotonicStack._nextElement(nums, True)
 
-nums = [1, 3, 4, 2, 5, 3, 4, 2]
+    @staticmethod
+    def nextGreaterElement(nums: list[int]) -> list[int | None]:
+        return MonotonicStack._nextElement(nums, False)
 
-print(MonotonicStack.nearestSmallerElements(nums))
+
+nums = [1, 2, 6, 9, 3, 4, 7, 5, 4, 2]
+
+print(MonotonicStack.nextSmallerElement(nums))  # [None, None, 3, 3, 2, 2, 5, 4, 2, None]
+print(MonotonicStack.nextGreaterElement(nums))  # [2, 6, 9, None, 4, 7, None, None, None, None]
